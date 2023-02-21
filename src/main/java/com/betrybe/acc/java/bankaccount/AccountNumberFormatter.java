c5797659ca6b2aaa5c3a531d00c1e838dd320174
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-02-21 20:58:58 -0300

    Implementação do método de formatação de numero utilizando a dependencia Apache Commons Lang

--- src/main/java/com/betrybe/acc/java/bankaccount/AccountNumberFormatter.java
+++ src/main/java/com/betrybe/acc/java/bankaccount/AccountNumberFormatter.java
@@ -1,18 +1,27 @@
 package com.betrybe.acc.java.bankaccount;
 
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * AccountNumberFormatter.
  */
 public class AccountNumberFormatter {
+  private static final int NUM_MAX = 6;
+  private static final String CHAR_ESPECIAL = "0";
 
   /**
+   * Classe formatAccountNumber
    * Formata o número da conta.
    *
    * @param literalAccountNumber número da conta
    */
   public String formatAccountNumber(int literalAccountNumber) {
-
+    String stringNumero = Integer.toString(literalAccountNumber);
+    
+    if (stringNumero.length() > 6) {
+      return StringUtils.right(stringNumero, NUM_MAX);
+    }
+    return StringUtils.leftPad(stringNumero, NUM_MAX, CHAR_ESPECIAL);
   }
 
 }
