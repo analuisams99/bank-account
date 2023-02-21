ef0ef5f21a4fb129b5e2482aaef5a86fdb770948
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-02-21 20:59:40 -0300

    Implementação do método principal informando numero da conta

--- src/main/java/com/betrybe/acc/java/bankaccount/Application.java
+++ src/main/java/com/betrybe/acc/java/bankaccount/Application.java
@@ -1,15 +1,27 @@
 package com.betrybe.acc.java.bankaccount;
 
-/**
- * A class.
- */
+import java.util.Scanner;
+
+/**Classe Application. */
 public class Application {
 
-  /**
-   * A simple get method.
-   */
+  /**Método principal. */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    AccountNumberFormatter accountNumberFormatter = new AccountNumberFormatter(); 
+    System.out.println("Informe o número da conta: ");
+    
+    try {
+      int numeroConta = scanner.nextInt();
+      String numeroContaFormatado = accountNumberFormatter.formatAccountNumber(numeroConta);
+      System.out.println("Numero da conta: " + numeroContaFormatado);
 
+    } catch (Exception e) {
+      System.out.println("Numero da conta inválido!");
+
+    } finally {
+      scanner.close();
+    }
   }
 
 }
